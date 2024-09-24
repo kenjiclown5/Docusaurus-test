99048341b1be07437a43a480d099ac9f79c571a7
Kenji Ikehara <kenji.clown@gmail.com>
2024-09-24 11:52:22 +0900

    modified.

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -2,11 +2,15 @@ import React from 'react';
 // Import the original mapper
 import MDXComponents from '@theme-original/MDXComponents';
 import Badge from '@site/src/components/Badge2';
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
 import { Icon } from '@iconify/react'; // Import the entire Iconify library.
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
   Badge,
+  Tabs,
+  TabItem,
   IIcon: Icon, // Make the iconify Icon component available in MDX as <icon />.
 };
